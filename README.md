2fc8b29a26fad8f74974a354abf0786abfc07d25
Rocard <rocard@optimiz.claims>
2021-10-06 20:05:54 +0100

    complet working demo

---
+++ README.md
@@ -0,0 +1,2 @@
+# Crypto payments with Coinbase + Firebase
+Accept crypto payments in your web app with [Coinbase Commerce](https://commerce.coinbase.com) & Firebase Cloud Functions.
---
+++ functions/index.js
@@ -0,0 +1,65 @@
+const functions = require('firebase-functions');
+const cors = require('cors')({ origin: '*' });
+
+const { Client, Webhook, resources } = require('coinbase-commerce-node');
+const coinbaseSecret = '9e6211dd-3aff-4acc-afd1-aa82f64a1517';
+
+Client.init(coinbaseSecret);
+
+const { Charge } = resources;
+
+exports.createCharge = functions.https.onRequest((req, res) => {
+    cors(req, res, async () => {
+      // TODO get product data from database
+  
+      const chargeData = {
+        name: 'Widget',
+        description: 'Payment widget created by Rocard LLC',
+        local_price: {
+          amount: 9.99,
+          currency: 'USD',
+        },
+        pricing_type: 'fixed_price',
+        metadata: {
+          user: 'jeffd23',
+        },
+      };
+  
+      const charge = await Charge.create(chargeData);
+      console.log(charge);
+      
+      // user has one hr to make payment before expires
+      res.send(charge);
+    });
+  });
+
+  exports.webhookHandler = functions.https.onRequest(async (req, res) => {
+    const rawBody = req.rawBody;
+    const signature = req.headers['x-cc-webhook-signature'];
+    const webhookSecret = '';
+  
+    try {
+      const event = Webhook.verifyEventBody(rawBody, signature, webhookSecret);
+  
+      if (event.type === 'charge:pending') {
+        // TODO
+        // user paid, but transaction not confirm on blockchain yet
+      }
+  
+      if (event.type === 'charge:confirmed') {
+        // TODO
+        // all good, charge confirmed
+      }
+  
+      if (event.type === 'charge:failed') {
+        // TODO
+        // charge failed or expired
+      }
+  
+      res.send(`success ${event.id}`);
+      
+    } catch (error) {
+      functions.logger.error(error);
+      res.status(400).send('failure!');
+    }
+  });
